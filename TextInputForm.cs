a0fd249ffcd054daed551b910043254e5e983d21
Nickolas-Duong <nduong@cpp.edu>
2025-05-05 11:30:38 -0700

    Create TextInputForm.cs
    For text input form

---
+++ TextInputForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace _4080capstone
+{
+    public partial class TextInputForm : Form
+    {
+        public string UserInput { get; private set; }
+
+        public TextInputForm(string existingText = "")
+        {
+            InitializeComponent();
+            txtEditor.Text = existingText;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            UserInput = txtEditor.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
